1441ee86466276d72477f86cc453f2965f9ac661
khan faysal <mdfaysalkhancse@gmail.com>
2023-08-29 18:09:55 +0600

    student single delete successfully done

--- src/app/modules/student/student.routes.ts
+++ src/app/modules/student/student.routes.ts
@@ -27,7 +27,7 @@ router.patch(
 
 router.delete(
     '/:id',
-    auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+    // auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
     StudentController.deleteFromDB);
 
 export const studentRoutes = router;
