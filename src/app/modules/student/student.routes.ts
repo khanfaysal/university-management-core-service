e6b1e05ae866717434623078acae241ad8405424
khan faysal <mdfaysalkhancse@gmail.com>
2023-08-29 17:32:55 +0600

    student single update successfully done

--- src/app/modules/student/student.routes.ts
+++ src/app/modules/student/student.routes.ts
@@ -21,7 +21,7 @@ router.post(
 
 router.patch(
     '/:id',
-    auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+    // auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
     validateRequest(StudentValidation.update),
     StudentController.updateIntoDB)
 
